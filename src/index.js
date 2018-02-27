b151038afb008d5510f75a49ac6e3bb6275b3e4c
Alex Brov <shalak10@outlook.com>
2018-02-27 14:35:56 +0300

    implementation of sortByProto

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = (propertyName) => {return propertyName};
-const createNotEnumerableProperty = (propertyName) => {return Symbol(propertyName)};
-const createProtoMagicObject = () => {
-};
+const createEnumerableProperty = (propertyName) => { return propertyName };
+const createNotEnumerableProperty = (propertyName) => { return Symbol(propertyName) };
+const createProtoMagicObject = () => {};
 
-let value=0;
+let value = 0;
 const incrementor = () => {
-value++;
-return incrementor;
+    value++;
+    return incrementor;
 };
-Function.prototype.toString=()=>{
+Function.prototype.toString = () => {
     return value;
 };
 
-let count=0;
+let count = 0;
 const asyncIncrementor = () => {
     return new Promise((resolve) => {
-       resolve(++count);
+        resolve(++count);
     })
 };
 
 const createIncrementer = () => {
-    let count=0;
-   function* Incrementor() {
-       while(true){
-        yield ++count;
-       }
+    let count = 0;
+
+    function* Incrementor() {
+        while (true) {
+            yield ++count;
+        }
     }
     return Incrementor();
 
@@ -35,27 +35,37 @@ const createIncrementer = () => {
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = (param) => {
     return new Promise((resolve) => {
-    setTimeout(()=>{resolve(param);},1052)
-})};
+        setTimeout(() => { resolve(param); }, 1052)
+    })
+};
 const getDeepPropertiesCount = (obj) => {
-let count=0,i=0;
+    let count = 0,
+        i = 0;
 
-    temp=obj;
-    while(Object.values(temp).length!=0){
-        count+=Object.values(temp).length;
-        count+=Object.values(temp[i]).length;
-        temp=temp[i][i];
+    temp = obj;
+    while (Object.values(temp).length != 0) {
+        count += Object.values(temp).length;
+        count += Object.values(temp[i]).length;
+        temp = temp[i][i];
         i++;
-        if(i>99){
-            i=0;
+        if (i > 99) {
+            i = 0;
         }
     }
     return count;
 };
 
 const createSerializedObject = () => {};
-const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (arr) => {
+  arr.sort((a,b)=>{
+      if(a.__proto__<b.__proto__){
+          return -1;
+      }else{
+          return 1;
+      }
+  });
+    return arr;
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
